15c41d6f3cabc2d1669a0afddcde0596896c0178
EnvYend Ricardo YanHua <ricardoyanhua@gmail.com>
2025-04-26 14:41:01 -0500

    Nuevo Proyecto - Declaracion del nombre del proyecto

---
+++ src/main/java/com/unu/web/GestorEmpleadoApplication.java
@@ -0,0 +1,13 @@
+package com.unu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEmpleadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEmpleadoApplication.class, args);
+	}
+
+}
